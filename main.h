85d83c2411293a47b151cac66f966a11caed7654
Maria Esmeralda <6580@holbertonstudents.com>
2023-07-02 04:20:47 -0700

    backup

--- main.h
+++ main.h
@@ -2,7 +2,21 @@
 #define _PRINTF_H_
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
+/**
+ * struct print_struct - struct to print args
+ * @op: operator
+ * @f: Pointer to function to call
+ */
+typedef struct print_struct
+{
+	char *op;
+	int (*f)();
+} print_t;
+int _putchar(char c);
 int _printf(const char *format, ...);
+int printf_char(va_list box);
+int printf_string(va_list box);
+int printf_integer(va_list box);
 #endif
--- test/main.c
+++ test/main.c
@@ -6,9 +6,11 @@ int main(void)
         int len2;
 
         len = _printf("Let's try to %s a %s %s.\n", "printf", "simple", "sentence");
-        len2 = _printf("Let's try to printf a simple sentence.\n");
+        len2 = printf("Let's try to printf a simple sentence.\n");
         _printf("String:[%s]\n", "I am a string !");
         _printf("Character:[%c]\n", 'H');
-	_printf("Length:[%d, %i]\n", len, len2);
+        printf("Character:[%c]\n", 'H');
+	_printf("Length:[%d, %i]\n", len, len);
+    	printf("Length:[%d, %i]\n", len2, len2);
         return (0);
 }
